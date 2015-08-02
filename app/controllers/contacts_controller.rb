7741c945e5600e82629ea6189f032c0c311cb9b5
Angel Renta <arenta09@mail.bw.edu>
2015-08-02 03:40:32 +0000

    Made alterations to attempt to fix heroku problems

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,21 +2,20 @@ class ContactsController < ApplicationController
     def new
         @contact = Contact.new
     end
-    
-    def create
-        @contact = Contact.new(contact_params)
         
-        if @contact.save
-            name = params[:contact][:name]
-            email = params[:contact][:email]
-            body = params[:contact][:comment]
-            ContactMailer.contact_email(name, email, body).deliver
-            flash[:success] = "Message Sent."
-            redirect_to new_contact_path
-    else
-        flash[:danger] = "An error occured. Message not sent."
+      def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+        name = params[:contact][:name]
+        email = params[:contact][:email]
+        body = params[:contact][:comments]
+        ContactMailer.contact_email(name, email, body).deliver
+        flash[:success] = 'Message sent.'
         redirect_to new_contact_path
-        end
+      else
+        flash[:danger] = 'Error occured, message has not been sent.'
+        redirect_to new_contact_path
+      end
     end
     
     private
