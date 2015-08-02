12bb5e3d4a4d603a31eb67eec1d92c40c28005a7
Angel Renta <arenta09@mail.bw.edu>
2015-08-02 00:34:32 +0000

    fixed application.css

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,6 +21,3 @@ module SimplecodecastsSaas
     # config.i18n.default_locale = :de
   end
 end
-
-config.assets.initialize_on_precompile = false
-
